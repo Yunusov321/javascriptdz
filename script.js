c05971a473ccc1ac1ff87e7243be37c6017c33be
Yunusov321 <diloromsaidova93@gmail.com>
2024-12-12 18:39:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var abdulloh = 5;
+let islomjons = 'Miya yo';
+const zarinaManti = 155;
+
+
+let userName = prompt('Ismingizni kiriting:')
+console.log('Salom, ${Abdulaziz}! \ xush kelibsiz');
+let age = prompt('Yoshingizni kiriting:');
+let name = prompt('Mahallangizni kiriting:');
